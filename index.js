cdfe7b710c3e6d5350e87f01a68841062e3de222
Authoi234 <jawadkhan.authoi@gmail.com>
2024-09-18 23:53:22 +0600

    data of videos and videos by id is ready

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
 const app = express();
+const videos = require('./videos.json');
+const videosData = require('./videosData.json');
+
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +12,16 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('As Shakil Website Is successfully running');
 });
+
+app.get('/videos/:id', async(req, res) => {
+    const id = req.params.id;
+    const video = videos.find(singleVideo => singleVideo.id === Number(id));
+    res.send(video);
+})
+
+app.get('/videos/', async(req, res) => {
+    res.send(videosData);
+})
 
 
 

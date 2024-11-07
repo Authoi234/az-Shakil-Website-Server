634345e313864e905eaa43a68e0f6fca1cc3d31a
Authoi234 <jawadkhan.authoi@gmail.com>
2024-11-07 20:56:16 +0600

    updated

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-const videos = require('./videos.json');
-const videosData = require('./videosData.json');
+const fs = require('fs');
+const path = require('path');
 
+const readJSONFile = (filename) => {
+    const filePath = path.join(__dirname, 'public', filename);
+    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+};
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    
 
 app.get('/', (req, res) => {
     res.send('As Shakil Website Is successfully running');
 });
 
 app.get('/videos/:id', async(req, res) => {
+    const videos = readJSONFile('videos.json');
     const id = req.params.id;
     const video = videos.find(singleVideo => singleVideo.id === Number(id));
     res.send(video);
 })
 
 app.get('/videos/', async(req, res) => {
+    const videosData = readJSONFile('videosData.json');
     res.send(videosData);
 })
 
